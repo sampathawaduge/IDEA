6b26a8e704f385bb00f502779d698312240116b9
sameera <sampathawaduge94@gmail.com>
2016-04-16 08:24:01 +0530

    js
    javascript file

---
+++ assets/js/submit.js
@@ -0,0 +1,76 @@
+/**
+ * Created by AppleFactory on 4/16/2016 AD.
+ */
+$(document).ready(function(){
+    $('#summernote').summernote();
+
+    var selection;
+
+    $(".dropdown-menu li a").click( function() {
+        selection = $(this).text();
+
+    });
+
+    var today = new Date();
+    var dd = today.getDate();
+    var mm = today.getMonth()+1; //January is 0!
+
+    var yyyy = today.getFullYear();
+    if(dd<10){
+        dd='0'+dd
+    }
+    if(mm<10){
+        mm='0'+mm
+    }
+    var today = dd+'/'+mm+'/'+yyyy;
+    var dt=new Date();
+    var hours=dt.getHours();
+    var min;
+    if(hours >=12)
+    {
+        var minutes=dt.getMinutes();
+        var min=hours+'.'+minutes+' PM';
+    }
+    else
+    {
+        var minutes=dt.getMinutes();
+        var min=hours+'.'+minutes+' AM';
+
+    }
+
+
+    $("#idea_button").click(function(){
+
+        var comment = $('#summernote').summernote('code');
+
+        if(comment && selection)
+        {
+
+            $.ajax({
+
+                url:"http://localhost:81/IDEA/index.php/add_submission/show",
+                type:"POST",
+                data:{min:min,date:today,comment:comment,subcat:selection},
+                success:function(data)
+                {
+                    alert("Submission add successfully!");
+                    location.reload();
+                }
+            });
+        }
+        else if(!comment)
+        {
+            alert("Submission Is Empty");
+        }
+        else if(!selection)
+        {
+            alert("Select Submission category");
+        }
+        console.log(today);
+    })
+
+    $(".link").click(function(){
+        window.location.href="http://localhost:81/IDEA/index.php/Login_controller/mainpage";
+
+    });
+});
