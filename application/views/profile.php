49c4962ff15d39ea3850ad991b3c5842e45d0217
PjCastila <ccastila@yahoo.com>
2016-05-17 15:48:25 +0530

    Update for sprint 3
    Notification and profile picture

--- application/views/profile.php
+++ application/views/profile.php
@@ -1,9 +1,193 @@
+<script src="http://code.jquery.com/jquery-1.11.1.min.js"></script>
 
+<script src="http://cdn.jsdelivr.net/jquery.validation/1.15.0/jquery.validate.min.js"></script>
+<script src="http://cdn.jsdelivr.net/jquery.validation/1.15.0/additional-methods.min.js"></script>
+<script type="text/javascript">
+
+
+
+    function addmsg(type, msg){
+
+        $('#notification_count').html(msg);
+
+    }
+
+
+    function addmsginfo(type, msg){
+
+
+        //  $('#menu1').html(msg);
+
+    }
+
+    function waitForMsg(){
+        $.ajax({
+
+            type: "POST",
+            url: "<?php echo base_url(); ?>" + "index.php/user_profile/user_data_submit",
+            async: true,
+            cache: false,
+            timeout:50000,
+
+            success: function(data){
+                console.log(data);
+
+                //  obj = JSON.parse(data);
+                //  console.log(obj.notification);
+                addmsg("new", data);
+                setTimeout(
+                    waitForMsg,
+                    1000
+                );
+            },
+            error: function(XMLHttpRequest, textStatus, errorThrown){
+                addmsg("error", textStatus + " (" + errorThrown + ")");
+                setTimeout(
+                    waitForMsg,
+                    15000);
+            }
+        });
+    };
+
+
+
+    function waitForMsgInfo(){
+        $.ajax({
+
+            type: "POST",
+            url: "<?php echo base_url(); ?>" + "index.php/user_profile/msg_data",
+            async: true,
+            cache: false,
+            timeout:50000,
+
+            success: function(message){
+                console.log(message);
+                obj = JSON.parse(message);
+                console.log(obj);
+
+
+
+
+// JSONArray mArray;
+//         try {
+//             mArray = new JSONArray(responseString);
+//              for (int i = 0; i < mArray.length(); i++) {
+//                     JSONObject mJsonObject = mArray.getJSONObject(i);
+//                     Log.d("OutPut", mJsonObject.getString("NeededString"));
+//                 }
+//         } catch (JSONException e) {
+//             e.printStackTrace();
+//         }
+
+
+
+
+                var out = "";
+                var i;
+                for(i = 0; i < obj.length; i++) {
+                    out += '<li>new comment ' + obj[i].notification + ' on submission ' + obj[i].submission_id + '</li><br>';
+                }
+                //    out += '<a href="">View all comments</a>';
+                //   out += '<a  class="btn btn-success" id="mark_read">Mark All As Read</a>'
+                document.getElementById("menu1").innerHTML = out;
+
+
+                // echo "<a href='".site_url('comment/'.$submission->submission_id)."'>";
+
+
+
+                //   addmsginfo("new", data);
+                setTimeout(
+                    waitForMsg,
+                    1000
+                );
+            },
+            error: function(XMLHttpRequest, textStatus, errorThrown){
+                addmsg("error", textStatus + " (" + errorThrown + ")");
+                setTimeout(
+                    waitForMsg,
+                    15000);
+            }
+        });
+    };
+
+    jQuery(function ($) {
+
+
+
+
+
+
+        $('#profil_upload').hide();
+        $( "#myform1" ).validate({
+            rules: {
+                //   nombre: { required: true, number: false, minlength: 1 },
+                //  email: { required: true, email: true },
+                //  cp: { required: true, number: true, minlength: 5, maxlength: 5 },
+                password: { required: true, minlength: 8 },
+                password2: { required: true, minlength: 8, equalTo: "#password" }
+            },
+            messages: {
+                //   nombre: "Introduce un nombre correcto",
+                //   email: "Introduce un email correcto",
+                //  cp: "Introduce un codigo postal correcto",
+                password: "passwrord should be atleast 8 characters",
+                password2: "passwords should match"
+            }
+        });
+
+        $("#edit_prof").click(function() {
+            $('#profil_upload').show();
+            console.log('sdasfadsfdsfsdfsdsdf');
+        });
+
+        /*$("#mark_read").click(function() {
+
+         console.log('sdasfasdf');
+         });
+
+
+
+         */
+        waitForMsg();
+        waitForMsgInfo();
+    });
+
+
+
+</script>
 
 <div class="container body">
 
 
     <div class="main_container">
+
+        <!-- Modal -->
+        <div id="myModal" class="modal fade" role="dialog">
+            <div class="modal-dialog">
+
+                <!-- Modal content-->
+                <div class="modal-content">
+                    <div class="modal-header">
+                        <button type="button" class="close" data-dismiss="modal">&times;</button>
+                        <h4 class="modal-title">Modal Header</h4>
+                    </div>
+                    <div class="modal-body">
+                        <?php echo  form_open_multipart('user_profile/uploadImage')?>
+
+                        <input type="file" name="userfile" />
+
+                        <p><input type="submit" name="submit" value="submit" /></p>
+
+                        <?php echo form_close();?>
+                    </div>
+                    <div class="modal-footer">
+                        <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+                    </div>
+                </div>
+
+            </div>
+        </div>
 
         <div class="col-md-3 left_col">
             <div class="left_col scroll-view">
@@ -16,7 +200,21 @@
                 <!-- menu prile quick info -->
                 <div class="profile">
                     <div class="profile_pic">
-                        <img src="<?php echo base_url('assets/images1/img.jpg')?>" alt="..." class="img-circle profile_img">
+                        <?php
+                        foreach ($details as $data)
+                        {
+
+                            $name = $data->name;
+                            $email = $data->email;
+                            $occupation = $data->occupation;
+                            $telephone = $data->telephone;
+                            $profile = $data->picture;
+
+                        }
+                        ?>
+                        <img src="<?=base_url(). 'images/' . $profile;?>" alt="..." class="img-circle profile_img" >
+
+
                     </div>
                     <div class="profile_info">
                         <span>Welcome,</span>
@@ -91,7 +289,7 @@
                     <ul class="nav navbar-nav navbar-right">
                         <li class="">
                             <a href="javascript:;" class="user-profile dropdown-toggle" data-toggle="dropdown" aria-expanded="false">
-                                <img src="<?php echo base_url('assets/images1/img.jpg')?>" alt="">
+                                <img src="<?=base_url(). 'images/' . $profile;?>" alt="">
                                 <?php
                                 echo $this->session->userdata['username'];
                                 ?>
@@ -102,6 +300,91 @@
                                 </li>
                             </ul>
                         </li>
+
+                        <li role="presentation" class="dropdown">
+                            <a href="javascript:;" class="dropdown-toggle info-number" data-toggle="dropdown" aria-expanded="false">
+                                <i class="fa fa-envelope-o"></i>
+                                <span id="notification_count" class="badge bg-green">6</span>
+                            </a>
+
+                        <div class="dropdown-menu list-unstyled msg_list animated fadeInDown" role="menu">
+                            <ul id="menu1">
+
+                                <li>
+                                    <a>
+                      <span class="image">
+                                        <img src="<?php echo base_url('assets/images1/img.jpg')?>" alt="Profile Image" />
+                                    </span>
+                      <span>
+                                        <span>John Smith</span>
+                      <span class="time">3 mins ago</span>
+                      </span>
+                      <span class="message">
+                                        Film festivals used to be do-or-die moments for movie makers. They were where...
+                                    </span>
+                                    </a>
+                                </li>
+                                <li>
+                                    <a>
+                      <span class="image">
+                                        <img src="<?php echo base_url('assets/images1/img.jpg')?>" alt="Profile Image" />
+                                    </span>
+                      <span>
+                                        <span>John Smith</span>
+                      <span class="time">3 mins ago</span>
+                      </span>
+                      <span class="message">
+                                        Film festivals used to be do-or-die moments for movie makers. They were where...
+                                    </span>
+                                    </a>
+                                </li>
+                                <li>
+                                    <a>
+                      <span class="image">
+                                        <img src="<?php echo base_url('assets/images1/img.jpg')?>" alt="Profile Image" />
+                                    </span>
+                      <span>
+                                        <span>John Smith</span>
+                      <span class="time">3 mins ago</span>
+                      </span>
+                      <span class="message">
+                                        Film festivals used to be do-or-die moments for movie makers. They were where...
+                                    </span>
+                                    </a>
+                                </li>
+                                <li>
+                                    <a>
+                      <span class="image">
+                                        <img src="<?php echo base_url('assets/images1/img.jpg')?>" alt="Profile Image" />
+                                    </span>
+                      <span>
+                                        <span>John Smith</span>
+                      <span class="time">3 mins ago</span>
+                      </span>
+                      <span class="message">
+                                        Film festivals used to be do-or-die moments for movie makers. They were where...
+                                    </span>
+                                    </a>
+                                </li>
+                                <li>
+                                    <div class="text-center">
+                                        <a>
+                                            <strong>See All Alerts</strong>
+                                            <i class="fa fa-angle-right"></i>
+                                        </a>
+                                    </div>
+                                </li>
+                            </ul>
+                            <ul class="text-center">
+
+                                <a href="<?php echo site_url('user_profile/msgread')?>" ><strong>Mark all as read</strong>
+                                    <i class="fa fa-angle-right"></i>
+                                </a>
+
+
+
+                            </ul>
+                        </div>
 
 
 
@@ -138,7 +421,7 @@
                                         <div id="crop-avatar">
                                             <!-- Current avatar -->
                                             <div class="avatar-view" title="Change the avatar">
-                                                <img src="<?php echo base_url('assets/images1/img.jpg')?>" alt="Avatar">
+                                                <img src="<?=base_url(). 'images/' . $profile;?>" alt="Avatar">
                                             </div>
 
 
@@ -176,7 +459,9 @@
 
                                     </ul>
 
-                                    <a class="btn btn-success"><i class="fa fa-edit m-right-xs"></i>Upload Profile Pictures</a>
+
+
+                                    <a class="btn btn-success" id="edit_prof" data-toggle="modal" data-target="#myModal"><i class="fa fa-edit m-right-xs"></i>Edit Profile</a>
                                     <br />
 
                                     <!-- start member type -->
